e85338394b16ef2a9dca2716b132a1b5ed5b25ba
Thetra00 <95437277+Thetra00@users.noreply.github.com>
2022-10-16 20:13:29 +0200

    EndGame
    Updated Scenemanager
    Events are updated
    AudioManagerFix
    MapChange
    Fixed all Event events.

---
+++ source/samhain-2/Assets/Thetra/Scripts/EndGameManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGameManager : MonoBehaviour
+{
+    public GameObject EndEvent;
+
+    void Start()
+    {
+        if(MapManager.instance.isLastBattle)
+            Instantiate(EndEvent);
+    }
+
+}
